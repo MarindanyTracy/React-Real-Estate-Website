7eb6c47df9af57cbd38f4ea41588bd182e6e6b74
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-12 11:36:46 +0300

    Initialization of project and some data we'll use

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,22 @@
+import { HiShieldCheck } from 'react-icons/hi';
+import { MdCancel, MdAnalytics } from 'react-icons/md';
+
+const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates in the market",
+    detail: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim"
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim"
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price in the market",
+    detail: "Exercitation in fugiat est ut ad ea cupidatat ut in cupidatat occaecat ut occaecat consequat est minim"
+  },
+];
+
+export default data;
