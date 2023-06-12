fd4da419cf679c37f185a3958ce2fc54aebbb386
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-12 13:21:27 +0300

    The Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
 
 function App() {
 
   return (
    <div className="App">
+    <div>
+      <div className="white-gradient" />
     <Header />
+    <Hero />
+    </div>
    </div>
   )
 }
