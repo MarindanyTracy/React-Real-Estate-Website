1672c1d4e4fd497ec504a978bc471673c4a1bdd4
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-12 13:28:54 +0300

    Companies logos

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Companies from './components/companies/Companies'
 
 function App() {
 
   return (
-   <div className="App">
+   <div className="App"> 
+
     <div>
       <div className="white-gradient" />
     <Header />
     <Hero />
     </div>
+    <Companies />
    </div>
   )
 }
