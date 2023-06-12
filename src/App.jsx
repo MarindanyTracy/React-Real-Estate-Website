1385c56c6464aa05cc887f161e260e76835c43ad
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-12 12:01:55 +0300

    Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
-   <h1>Hello</h1>
+   <div className="App">
+    <Header />
+   </div>
   )
 }
 
