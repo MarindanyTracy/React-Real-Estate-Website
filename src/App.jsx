23ef01065b1bb3da7ecdb630bc479738d6744c9c
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-13 10:02:01 +0300

    Value Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Residencies from './components/Residencies/Residencies'
 import Companies from './components/companies/Companies'
+import Value from './components/value/Value'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
     </div>
     <Companies />
     <Residencies />
+    <Value />
    </div>
   )
 }
